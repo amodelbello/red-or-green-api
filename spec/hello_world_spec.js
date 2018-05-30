14bc5f143a4298640a6868c4ad5e85346e73a44d
Amo DelBello <amodelbello@gmail.com>
2018-05-30 16:24:21 -0600

    Adds hello world test

---
+++ spec/hello_world_spec.js
@@ -0,0 +1,20 @@
+const request = require('request');
+const base_url = 'http://localhost:3000/';
+
+describe("Hello World Test", function(){
+  describe("GET /", function() {
+    it('returns status code 200', (done) => {
+      request.get(base_url, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    it("returns 'Welcome to Express'", (done) => {
+      request.get(base_url, function(err, res, body) {
+        expect(body).toContain("Welcome to Express");
+        done();
+      });
+    });
+  });
+});
